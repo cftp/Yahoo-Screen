1cc679d29f4b55a544a688d986993467dbc10ce7
Tarendai <contact@tomjn.com>
2014-01-07 12:43:18 +0000

    cache embed code for a day

--- yahoo-screen.php
+++ yahoo-screen.php
@@ -27,12 +27,18 @@ class CFTP_Yahoo_Screen {
 
 	function yahoo_embed_handler( $matches, $attr, $url, $rawattr ) {
 
-		$embed = sprintf(
-			'<figure class="o-container yahoo_screen">
-				<iframe src="%1$s?format=embed" frameborder="0" scrolling="no" width="650" height="450" marginwidth="0" marginheight="0" allowfullscreen></iframe>
-			</figure>',
-			esc_attr( $url )
-		);
+		$transient = get_transient( 'yahoo_screen_embed_'.$url );
+		$embed = $transient;
+		if ( $transient === false ) {
+			$embed = sprintf(
+				'<figure class="o-container yahoo_screen">
+					<iframe src="%1$s?format=embed" frameborder="0" scrolling="no" width="650" height="450" marginwidth="0" marginheight="0" allowfullscreen></iframe>
+				</figure>',
+				esc_attr( $url )
+			);
+			// we have a transient return/assign the results
+			set_transient( 'yahoo_screen_embed_'.$url, $embed, DAY_IN_SECONDS );
+		}
 
 		return apply_filters( 'embed_yahoo', $embed, $matches, $attr, $url, $rawattr );
 	}
