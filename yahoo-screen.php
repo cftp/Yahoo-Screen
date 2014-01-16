ac9aeb32f20f15485f72077e649ef0bc904e98a1
Tarendai <contact@tomjn.com>
2014-01-16 10:26:58 +0000

    Finally have the code working with a self hosted oembed provider, and back to feature parity with the original plugin

--- yahoo-screen.php
+++ yahoo-screen.php
@@ -9,11 +9,24 @@ License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-
+/**
+ * Class CFTP_Yahoo_Screen
+ */
 class CFTP_Yahoo_Screen {
 
+	/**
+	 * The primary instance of this object lives here
+	 *
+	 * @var null
+	 */
 	protected static $_instance = null;
 
+	/**
+	 * WordPress doesn't provide a place for this object to live, so rather than pollute the global namespace, we'll
+	 * use the singleton pattern to store it.
+	 *
+	 * @return CFTP_Yahoo_Screen|null
+	 */
 	public static function instance() {
 		if ( !isset( self::$_instance ) ) {
 			self::$_instance = new self();
@@ -21,37 +34,185 @@ class CFTP_Yahoo_Screen {
 		return self::$_instance;
 	}
 
+	/**
+	 * Adds the oembed providers
+	 */
 	public function __construct() {
-		wp_embed_register_handler( 'yahoo', '#(http|https)://(.+).screen.yahoo.com/(.+)#', array( $this, 'yahoo_embed_handler' ) );
-		wp_embed_register_handler( 'yahooscreen', '#(http|https)://screen.yahoo.com/(.+).html#', array( $this, 'yahoo_embed_handler' ) );
+		wp_oembed_add_provider( '#https?://(.+).screen.yahoo.com/(.+)#i', site_url('/?oembed=true&oembedtype=yahooscreen&format={format}'), true );
+		wp_oembed_add_provider( '#https?://screen.yahoo.com/(.+).html#i', site_url('/?oembed=true&oembedtype=yahooscreen&format={format}'), true );
 	}
 
-	function yahoo_embed_handler( $matches, $attr, $url, $rawattr ) {
-
-		$transient = false;//get_transient( 'yahoo_screen_embed_'.$url );
-		$embed = $transient;
-		if ( $transient === false ) {
-			$width = 640;
-			$height = 360;
-			$aspect = $width/$height;
-			global $content_width;
-			if ( isset( $content_width ) ) {
-				$width = $content_width;
-				$height = $content_width/$aspect;
-			}
-
-			$embed = sprintf(
-				'<figure class="o-container yahoo_screen">
-					<iframe src="%1$s?format=embed" frameborder="0" scrolling="no" width="%2$d" height="%3$d" marginwidth="0" marginheight="0" allowfullscreen></iframe>
-				</figure>',
-				esc_attr( $url ), $width, $height
-			);
-			// we have a transient return/assign the results
-			set_transient( 'yahoo_screen_embed_'.$url, $embed, DAY_IN_SECONDS );
+	/**
+	 * Generates a Yahoo Screen embed code given a URL
+	 *
+	 * @param $url A valid Yahoo Screen video URL to be embedded
+	 * @return string A Yahoo embed iframe
+	 */
+	public static function yahoo_embed_code( $url ) {
+		$width = 640;
+		$height = 360;
+		$aspect = $width/$height;
+		global $content_width;
+		if ( isset( $content_width ) ) {
+			$width = $content_width;
+			$height = $content_width/$aspect;
 		}
 
-		return apply_filters( 'embed_yahoo', $embed, $matches, $attr, $url, $rawattr );
+		$embed = sprintf(
+			'<figure class="o-container yahoo_screen">
+				<iframe src="%1$s?format=embed" frameborder="0" scrolling="no" width="%2$d" height="%3$d" marginwidth="0" marginheight="0" allowfullscreen></iframe>
+			</figure>',
+			esc_attr( $url ), $width, $height
+		);
+		return $embed;
 	}
 }
 
 add_action( 'init', array( 'CFTP_Yahoo_Screen', 'instance' ) );
+
+
+/**
+ * oEmbed Provider, modified code from Matthias and Craig
+ *
+ * @author Matthias Pfefferle
+ * @author Craig Andrews
+ * @author Tom J Nowell
+ */
+class YahooScreenOembedProvider {
+
+	/**
+	 * Initialises the provider by adding the necessary hooks
+	 */
+	public static function init() {
+		add_action( 'parse_query', array( 'YahooScreenOembedProvider', 'parse_query' ) );
+		add_filter( 'query_vars', array( 'YahooScreenOembedProvider', 'query_vars' ) );
+		add_filter( 'cftp_oembed_provider_data_yahoo_screen', array( 'YahooScreenOembedProvider', 'generate_default_content' ), 90, 3 );
+		add_action( 'cftp_oembed_provider_render_yahoo_screen_json', array( 'YahooScreenOembedProvider', 'render_json' ), 99, 2 );
+		add_action( 'cftp_oembed_provider_render_yahoo_screen_xml', array( 'YahooScreenOembedProvider', 'render_xml' ), 99 );
+	}
+
+	/**
+	 * adds query vars
+	 */
+	public static function query_vars( $query_vars ) {
+		foreach ( array( 'oembed', 'oembedtype', 'format', 'url', 'callback' ) as $qvar ) {
+			if ( !array_key_exists( $qvar, $query_vars ) ) {
+				$query_vars[] = $qvar;
+			}
+		}
+
+		return $query_vars;
+	}
+
+	/**
+	 * handles request
+	 */
+	public static function parse_query($wp) {
+		if (!array_key_exists('oembed', $wp->query_vars) ||
+			!array_key_exists('url', $wp->query_vars) ||
+			!array_key_exists('oembedtype', $wp->query_vars)
+		) {
+			return;
+		}
+
+		// we're only handling yahoo screen here
+		if ( $wp->query_vars['oembedtype'] != 'yahooscreen' ) {
+			return;
+		}
+
+		$yahoo_url = $wp->query_vars['url'];
+
+		// @TODO: perform a check on the regex if the URL matches to validate, if not, 404
+		/*if(!$post) {
+			header('Status: 404');
+			wp_die("Not found");
+		}*/
+
+		// add support for alternate output formats
+		$oembed_provider_formats = apply_filters( "oembed_provider_formats", array( 'json', 'xml' ) );
+
+		// check output format
+		$format = 'json';
+		if ( array_key_exists( 'format', $wp->query_vars ) && in_array( strtolower( $wp->query_vars['format'] ), $oembed_provider_formats ) ) {
+			$format = $wp->query_vars['format'];
+		}
+
+		// content filter
+		$oembed_provider_data = apply_filters( 'cftp_oembed_provider_data_yahoo_screen', array(), $yahoo_url );
+
+		do_action( 'cftp_oembed_provider_render_yahoo_screen', $format, $oembed_provider_data, $wp->query_vars);
+		do_action( "cftp_oembed_provider_render_yahoo_screen_{$format}", $oembed_provider_data, $wp->query_vars);
+	}
+
+	/**
+	 * adds default content
+	 *
+	 * @param array $oembed_provider_data
+	 * @param $url
+	 * @internal param string $post_type
+	 * @internal param Object $post
+	 *
+	 * @return array OEmbed data to be formatted as a response
+	 */
+	public static function generate_default_content( $oembed_provider_data, $url ) {
+		$oembed_provider_data['version'] = '1.0';
+		$oembed_provider_data['provider_name'] = 'Yahoo Screen';
+		$oembed_provider_data['provider_url'] = home_url();
+		$oembed_provider_data['author_name'] = 'Yahoo Screen';
+		$oembed_provider_data['author_url'] = 'http://screen.yahoo.com';
+		$oembed_provider_data['title'] = 'Yahoo Embed Title';
+		// @TODO: Grab the Yahoo video image and add it's URL and dimensions
+		/*if (function_exists('has_post_thumbnail') && has_post_thumbnail($post->ID)) {
+			$image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID));
+			$oembed_provider_data['thumbnail_url'] = $image[0];
+			$oembed_provider_data['thumbnail_width'] = $image[1];
+			$oembed_provider_data['thumbnail_height'] = $image[2];
+		}*/
+		$oembed_provider_data['type'] = 'rich';
+		$oembed_provider_data['html'] = CFTP_Yahoo_Screen::yahoo_embed_code( $url );
+
+		return $oembed_provider_data;
+	}
+
+	/**
+	 * Render json output
+	 *
+	 * @param array $oembed_provider_data
+	 * @param array $wp_query Query variables ( not a WP_Query object as you would think )
+	 */
+	public static function render_json($oembed_provider_data, $wp_query) {
+		header( 'Content-Type: application/json; charset=' . get_bloginfo( 'charset' ), true );
+
+		// render json output
+		$json = json_encode( $oembed_provider_data );
+
+		// add callback if available
+		if (array_key_exists( 'callback', $wp_query ) ) {
+			$json = $wp_query['callback'] . "($json);";
+		}
+
+		echo $json;
+		exit;
+	}
+
+	/**
+	 * Render xml output
+	 *
+	 * @param array $oembed_provider_data
+	 */
+	public static function render_xml( $oembed_provider_data ) {
+		header('Content-Type: text/xml; charset=' . get_bloginfo('charset'), true);
+
+		// render xml-output
+		echo '<?xml version="1.0" encoding="' . get_bloginfo('charset') . '" ?>';
+		echo '<oembed>';
+		foreach ( array_keys($oembed_provider_data) as $element ) {
+			echo '<' . $element . '>' . esc_html($oembed_provider_data[$element]) . '</' . $element . '>';
+		}
+		echo '</oembed>';
+		exit;
+	}
+
+}
+
+YahooScreenOembedProvider::init();
